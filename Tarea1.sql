353a2722e3da8bbbab1ff242b8dbf7592d4af7f7
Fl2on <69091361+fl2on@users.noreply.github.com>
2025-10-01 00:46:28 -0500

    Delete Tarea1.sql

--- Tarea1.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `estudiante` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) DEFAULT NULL,
-  `apellido` varchar(50) DEFAULT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `estado` int NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
